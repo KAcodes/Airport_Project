708f037d471422c3a7c6cc5b7e77fb80f6585633
“Grace” <graceibekwe97@gmail.com>
2024-02-08 12:35:52 +0000

    initial commit

--- frontend/src/components/atoms/button/StyledButton.jsx
+++ frontend/src/components/atoms/button/StyledButton.jsx
@@ -1,16 +1,39 @@
-import styled from 'styled-components';
-import { colors } from 'components/theme/colors';
+import styled from "styled-components";
+import { colors } from "components/theme/colors";
 
-export const StyledButton = styled.button`
+// export const StyledButton = styled.button`
+//   display: flex;
+//   color: ${(props) =>
+//     props.color === "primary" ? colors.primary.text : colors.secondary.text};
+//   background: ${(props) =>
+//     props.color === "primary"
+//       ? colors.primary.backgroundColor
+//       : colors.secondary.backgroundColor};
+
+//   border-width: 1px;
+//   border-radius: 5px;
+//   border-color: ${(props) =>
+//     props.color === "primary" ? colors.primary.text : colors.secondary.text};
+
+//   margin: 2px;
+//   padding: 5px;
+// `;
+
+export const StyledButton = ({ color }) => {
+  return styled.button`
     display: flex;
-    color: ${props => (props.color === 'primary' ? colors.primary.text : colors.secondary.text)};
-    background: ${props => (props.color === 'primary' ? colors.primary.backgroundColor : colors.secondary.backgroundColor)};
-    
+    color: ${color === "primary" ? colors.primary.text : colors.secondary.text};
+    background: ${color === "primary"
+      ? colors.primary.backgroundColor
+      : colors.secondary.backgroundColor};
+
     border-width: 1px;
     border-radius: 5px;
-    border-color: ${props => (props.color === 'primary' ? colors.primary.text : colors.secondary.text)};
+    border-color: ${color === "primary"
+      ? colors.primary.text
+      : colors.secondary.text};
 
     margin: 2px;
     padding: 5px;
-`;
-
+  `;
+};
