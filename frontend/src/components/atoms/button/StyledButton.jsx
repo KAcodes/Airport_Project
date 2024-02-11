338ed8a611fa62459d317e3a4392fb69c7233721
“Grace” <graceibekwe97@gmail.com>
2024-02-11 18:50:38 +0000

    update readme

--- frontend/src/components/atoms/button/StyledButton.jsx
+++ frontend/src/components/atoms/button/StyledButton.jsx
@@ -1,39 +1,39 @@
 import styled from "styled-components";
 import { colors } from "components/theme/colors";
 
-// export const StyledButton = styled.button`
-//   display: flex;
-//   color: ${(props) =>
-//     props.color === "primary" ? colors.primary.text : colors.secondary.text};
-//   background: ${(props) =>
-//     props.color === "primary"
-//       ? colors.primary.backgroundColor
-//       : colors.secondary.backgroundColor};
-
-//   border-width: 1px;
-//   border-radius: 5px;
-//   border-color: ${(props) =>
-//     props.color === "primary" ? colors.primary.text : colors.secondary.text};
-
-//   margin: 2px;
-//   padding: 5px;
-// `;
-
-export const StyledButton = ({ color }) => {
-  return styled.button`
-    display: flex;
-    color: ${color === "primary" ? colors.primary.text : colors.secondary.text};
-    background: ${color === "primary"
+export const StyledButton = styled.button`
+  display: flex;
+  color: ${(props) =>
+    props.color === "primary" ? colors.primary.text : colors.secondary.text};
+  background: ${(props) =>
+    props.color === "primary"
       ? colors.primary.backgroundColor
       : colors.secondary.backgroundColor};
 
-    border-width: 1px;
-    border-radius: 5px;
-    border-color: ${color === "primary"
-      ? colors.primary.text
-      : colors.secondary.text};
+  border-width: 1px;
+  border-radius: 5px;
+  border-color: ${(props) =>
+    props.color === "primary" ? colors.primary.text : colors.secondary.text};
 
-    margin: 2px;
-    padding: 5px;
-  `;
-};
+  margin: 2px;
+  padding: 5px;
+`;
+
+// export const StyledButton = ({color}) => {
+//   return styled.button`
+//     display: flex;
+//     color: ${color === "primary" ? colors.primary.text : colors.secondary.text};
+//     background: ${color === "primary"
+//       ? colors.primary.backgroundColor
+//       : colors.secondary.backgroundColor};
+
+//     border-width: 1px;
+//     border-radius: 5px;
+//     border-color: ${color === "primary"
+//       ? colors.primary.text
+//       : colors.secondary.text};
+
+//     margin: 2px;
+//     padding: 5px;
+//   `;
+// };
